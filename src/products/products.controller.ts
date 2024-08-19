1f0874519996acba4bd595f066a4389d855a6696
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-08-19 17:14:31 +0700

    Add logic for fetching product detail data

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Response } from 'express';
 
@@ -13,6 +13,32 @@ export class ProductsController {
       res
         .status(HttpStatus.OK)
         .json(await this.productsService.getHomepageProducts());
+    } catch (error) {
+      res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ ok: false, error: error.message });
+    }
+  }
+
+  @Get('slug-mapping')
+  async getProductSlugsMapping(@Res() res: Response) {
+    try {
+      res
+        .status(HttpStatus.OK)
+        .json(await this.productsService.getProductSlugsMapping());
+    } catch (error) {
+      res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ ok: false, error: error.message });
+    }
+  }
+
+  @Get(':slug')
+  async getProductBySlug(@Res() res: Response, @Param('slug') slug: string) {
+    try {
+      res
+        .status(HttpStatus.OK)
+        .json(await this.productsService.getProductBySlug(slug));
     } catch (error) {
       res
         .status(HttpStatus.INTERNAL_SERVER_ERROR)
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -27,4 +27,46 @@ export class ProductsService {
       products,
     };
   }
+
+  async getProductSlugsMapping(): Promise<{
+    ok: boolean;
+    productSlugsMapping: { [key: string]: string };
+  }> {
+    const products = await this.productModel.find().select('slug').lean();
+
+    const data = products.reduce((acc, product) => {
+      acc[product.slug.en] = product.slug.vi;
+      return acc;
+    }, {});
+
+    return {
+      ok: true,
+      productSlugsMapping: data,
+    };
+  }
+
+  async getProductBySlug(
+    slug: string,
+  ): Promise<{ ok: boolean; product: Product }> {
+    const product = await this.productModel
+      .findOne({
+        $or: [{ 'slug.en': slug }, { 'slug.vi': slug }],
+      })
+      .populate([
+        {
+          path: 'variants',
+          model: 'Variant',
+        },
+        {
+          path: 'category',
+          model: 'Category',
+          select: 'name slug',
+        },
+      ])
+      .lean();
+    return {
+      ok: true,
+      product,
+    };
+  }
 }
